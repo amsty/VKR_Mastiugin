fddad68f9b4d11f8651948c80ee86329c1fbbd5c
Alex <50638910+amsty@users.noreply.github.com>
2023-04-22 12:42:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import numpy as np
 a = np.arange(1,7, 0.5).reshape(3, 2, 2)
 print(a)
 
+import argparse
+#пишем обрабочтик команд из командной строки
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Example") #создали обработчик аргументов из терминала
+    parser.add_argument("first")
+    args = parser.parse_args()
+print(args.first)
 
